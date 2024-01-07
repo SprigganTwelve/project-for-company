ab1a4a5738cd91ae26db55ba54ab2b7d33b62c02
DALI <sika.dali@telecomnancy.net>
2024-01-07 16:08:22 +0100

    landscape navbar fixed

---
+++ LandPage/landpage.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Link Infinit</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Raleway:wght@400;500;700&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Raleway:wght@400;500;700&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="navbar">
+            <img src="../resources/logo_v1.svg" class="logo" height="100px">
+            <div class="right">
+                <div><a href="#">Home</a></div>
+                <div><a href="#">Contact</a></div>
+            </div>
+        </div>
+        <div class="content">
+            <div>
+                <h1> Internet Très Haut Débit</h1>
+                <p>L'internet sans limite depuis l'univers</p>
+            </div>
+            <div>
+                <button type="button"> Rejoindre </button>
+                <button type="button"> En savoir plus </button>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
